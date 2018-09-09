01158209652e08864f3c8fb43f3be92db94065aa
Jose Stricklin <josestricklin@El-Diablo.local>
2018-09-08 23:07:31 -0600

    improve responsive design

--- roverwatch-vue/src/components/Info.vue
+++ roverwatch-vue/src/components/Info.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main-content p-5">
         <h2 class="main-title text-center pb-1">Info</h2>
-        <p><span id="bold">RoverWatch</span> was built utilizing simple HTML, CSS, JavaScript, and API calls to NASA's databse for the rover images</p>
+        <p><span id="bold">RoverWatch</span> was built utilizing Vue.js with Vue Router and Bootstrap</p>
     </div>
 </template>
 <script>
